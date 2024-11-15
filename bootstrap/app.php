471165626a6891c8013d40805a3c7e1c83dab881
Krzysztof Karaś <krzychu12350@interia.pl>
2024-11-15 22:58:33 +0100

    Add not found handling middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\ForceJsonApiResponse;
+use App\Http\Middleware\HandleNotFound;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(ForceJsonApiResponse::class);
+        $middleware->append(HandleNotFound::class);
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
