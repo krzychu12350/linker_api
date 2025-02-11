b9521c768b31fabac7bd0e77312642bd53ea6d79
Krzysztof Karaś <krzychu12350@interia.pl>
2025-02-11 05:50:58 +0100

    Update config/database.php for pgsql

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,27 +22,27 @@ class DatabaseSeeder extends Seeder
 //            'name' => 'Test User',
 //            'email' => 'test@example.com',
 //        ]);
-//        $this->call([
-//           RolesAndPermissionsSeeder::class,
-//            UserDetailSeeder::class,
-//        ]);
-//
-////        User::factory(20)->withRole('user')->create();
-//        User::factory(20)->withRole('moderator')->create();
-//        User::factory(1)->withRole('user')->create([
-//            'first_name' => 'Test',
-//            'last_name' => 'Test',
-//            'email' => 'TEST@TEST.pl',
-//            'password' => Hash::make('securepassword123')
-//        ]);
-//        User::factory(1)->withRole('user')->create([
-//            'first_name' => 'Test2',
-//            'last_name' => 'Test2',
-//            'email' => 'TEST2@TEST.pl',
-//            'password' => Hash::make('securepassword123')
-//        ]);
-//
-//        Report::factory(20)->create();
+        $this->call([
+           RolesAndPermissionsSeeder::class,
+            UserDetailSeeder::class,
+        ]);
+
+//        User::factory(20)->withRole('user')->create();
+        User::factory(20)->withRole('moderator')->create();
+        User::factory(1)->withRole('user')->create([
+            'first_name' => 'Test',
+            'last_name' => 'Test',
+            'email' => 'TEST@TEST.pl',
+            'password' => Hash::make('securepassword123')
+        ]);
+        User::factory(1)->withRole('user')->create([
+            'first_name' => 'Test2',
+            'last_name' => 'Test2',
+            'email' => 'TEST2@TEST.pl',
+            'password' => Hash::make('securepassword123')
+        ]);
+
+        Report::factory(20)->create();
         $this->call([
             EventPollSeeder::class,
         ]);
